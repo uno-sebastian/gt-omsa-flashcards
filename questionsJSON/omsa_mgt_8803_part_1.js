678488bc6660d228156fb3e37c21112023030e7e
Seabass <uno.sebastian.echo@gmail.com>
2024-05-16 23:14:46 -0400

    Create omsa_mgt_8803_part_1.js

---
+++ questionsJSON/omsa_mgt_8803_part_1.js
@@ -0,0 +1,32 @@
+questionSets.push({
+    "name": "Business Fundamentals for Analytics - Week 1: Introduction to Financial Accounting",
+    "data": [
+        // Week 1: What is Financial Accounting?
+        { "Front": "What is the purpose of financial accounting", "Back": "To provide meaningful quantitative financial information regarding an entity’s activities to decision makers" },
+        // Who are these decision makers?
+        { "Front": "Who are Shareholders within Financial Accounting?", "Back": "Individuals or groups who are in charge/own the business" },
+        { "Front": "Who are Debt holders within Financial Accounting?", "Back": "Decision makers: Individuals or groups who are crediting money for the business" },
+        { "Front": "Who are Managers within Financial Accounting?", "Back": "Decision makers: Not primarily responsible for financial information" },
+        { "Front": "Who are Investment analysts and information intermediaries within Financial Accounting?", "Back": "Decision makers: Experts in financial analytics field" },
+        { "Front": "Who are Customers and strategic partners within Financial Accounting?", "Back": "Decision makers: Could use financial information, but not the main focus" },
+        // What Makes Information Useful?
+        { "Front": "Information Relevance", "Back": "Capable of making a difference to a decision maker" },
+        { "Front": "Faithful Representation", "Back": "Dependable and reliable" },
+        // Where Do Financial Accounting Rules (Standards) Come From?
+        { "Front": "What does SEC stand for?", "Back": "The Securities and Exchange Commission" },
+        // Financial Accounting is not an Exact Science
+        { "Front": "What does GAAP stand for?", "Back": "Generally Accepted Accounting Principles" },
+        { "Front": "Why are financial statements not exact?", "Back": "Financial statements depend on countless estimates" },
+        // Oversight of Financial Accounting
+        { "Front": "What does AICPA stand for?", "Back": "American institute of Certified Public Accountants" },
+        { "Front": "What are some tools to oversee the bias in financial accounting?", "Back": "SEC, AICPA, and Securities Law" },
+        // Accounting and Capital Allocation
+        { "Front": "What does AICPA stand for?", "Back": "American institute of Certified Public Accountants" },
+        // Required Financial Statements
+        { "Front": "What are the 5 required financial statements?", "Back": "• Balance Sheet\n• Income Statement\n• Statement of Shareholders’ Equity\n• Statement of Cash flows\n• Statement of Comprehensive Income" },
+        { "Front": "What are the 2 most useful required financial statements?", "Back": "Balance Sheet and Income Statement" },
+        // Information Beyond Financial Statements
+        { "Front": "What financial statements are NOT required?", "Back": "• Financial Statement Footnotes\n• Independent Auditor Report\n• Management Discussion and Analysis (MD&A)\n• Press Releases" },
+    ]
+})
+// { "Front": "", "Back": "" }
